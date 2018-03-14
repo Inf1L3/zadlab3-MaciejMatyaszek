ccb3cca96eb48c2cc5ce5b4d71a10d5aa4263b9c
you@example.com <you@example.com>
2018-03-14 17:37:04 +0100

    Zadanialab

--- POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
+++ POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
@@ -8,14 +8,37 @@ package pl.edu.ur.POLab3;
 /**
  *
  * @author Asia
- */
+ */import java.util.*; 
 public class Main {
-
+public static int silnia(int wartosc) {
+// jeśli przekazany parametr jest równy zero to zwróć 1
+// a w przeciwnym wypadku zwróć wartość parametru * wywołanie metody silnia
+// z parametrem o jeden mniejszym
+if(wartosc == 0) return 1;
+else return wartosc * silnia(wartosc - 1);
+}
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO 1.4, 1.7, 
+        Scanner sc = new Scanner(System.in);
+System.out.print("Podaj liczbę: " ); 
+// pobieramy od użytkownika liczbę
+int liczba = sc.nextInt(); 
+// wyświetlamy na ekranie obliczoną silnię
+System.out.println(liczba + "! = " + silnia(liczba));
+   int n = sc.nextInt();
+   
+   int wynik = 1;
+    int i;
+ for (i=1; i<n; i++) {
+     wynik*=i;
+     
+ }
+        System.out.println(wynik+"");
+ 
+ 
+          // TODO 1.4, 1.7, 
         // TODO 2.2
         // TODO 3.3, 3.4
     }
